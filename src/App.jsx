1287bf1a72d6acb49a82673b2b1afde95252fcef
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-12-16 12:59:50 +0530

    Upgraded to React 19 from rc

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-center p-4 m-4">Start Coding in React 19</h1>
-    </>
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
+      <p className="bg-gray-300 p-2 rounded text-3xl">
+        <a 
+          href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
+          target="_blank"
+          className="underline mx-1">Learn React 19 With Code</a>
+        <span>▶️</span>  
+      </p>
+    </div>
   );
 }
 

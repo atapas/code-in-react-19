4842c27f070f4181e94754979e5f46ad7f4fab37
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-01-25 14:54:33 +0530

    Upgraded to tailwindCSS 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
-      <p className="bg-gray-300 p-2 rounded text-3xl">
+      <p className="bg-gray-300 p-2 rounded-sm text-3xl">
         <a 
           href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
           target="_blank"
